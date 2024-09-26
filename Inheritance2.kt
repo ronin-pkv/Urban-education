e67190e109e35d0289df8c34783f59922c7ae7dd
ronin-pkv <ronin.krp@gmail.com>
2024-09-26 15:15:14 +0300

    Add files via upload
    Дан массив целых чисел 3, 67, 1, 55, 65, 89, 23. Вывести на экран элементы массива, которые делятся на 5 на цело.

---
+++ Inheritance2.kt
@@ -0,0 +1,11 @@
+package Kotlin.Inheritance
+
+fun main() {
+    val numbers = arrayOf(3, 67, 1, 55, 65, 89, 23)
+
+    for (num in numbers) {
+        if (num % 5 == 0) {
+            println(num)
+        }
+    }
+}
