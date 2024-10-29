35f13a5669c7cd17c0bbfef403e8b3dc7271945e
ronin-pkv <ronin.krp@gmail.com>
2024-10-29 14:04:22 +0300

    Add files via upload
    Дан список целых чисел. Вывести в консоль первый элемент списка двумя способами: обратившись по индексу, через стандартную функцию first().

---
+++ List0.kt
@@ -0,0 +1,7 @@
+package Kotlin.Collections
+
+fun main() {
+    val list1 = listOf(1, 2, 3, 4, 5, 6, 7, 8)
+    println("Первый элемент по индексу: ${list1[0]}")
+    println("Первый элемент (first): ${list1.first()}")
+}
